c16131592801143aaee682531dd664a40acc19ee
kal <Haikalhelmy13@Gmail.com>
2024-12-18 10:04:42 +0700

    Mobile Navbar

--- components/navbar/MobileNav.php
+++ components/navbar/MobileNav.php
@@ -1,24 +1,27 @@
 <!-- components/navbar/MobileNav.php -->
-<div class="mobile-menu" id="mobileMenu">
-    <div class="menu-backdrop"></div>
-    <div class="menu-content">
-        <div class="menu-header">
-            <div class="menu-brand">
-                <img src="../assets/images/logo.jpeg" alt="Logo" width="32" height="32">
-                <span>Historical Jogja</span>
+
+    <!-- Mobile Menu Container -->
+    <div class="mobile-menu" id="mobileMenu">
+        <div class="menu-backdrop"></div>
+        <div class="menu-content">
+            <div class="menu-header">
+                <div class="menu-brand">
+                    <img src="../assets/images/logo.jpeg" alt="Logo" width="32" height="32">
+                    <span>Historical Jogja</span>
+                </div>
+                <button class="close-menu" aria-label="Close menu">
+                    <i class="fas fa-times"></i>
+                </button>
             </div>
-            <button class="close-menu" aria-label="Close menu">
-                <i class="fas fa-times"></i>
-            </button>
+            <nav class="menu-links">
+                <a href="beranda.php" class="menu-link">Beranda</a>
+                <a href="bbsejarah.php" class="menu-link">Bangunan Bersejarah</a>
+                <a href="gallery.php" class="menu-link">Galeri</a>
+                <a href="timeline.php" class="menu-link">Timeline</a>
+                <a href="quiz.php" class="menu-link">Kuis</a>
+                <a href="petawisata.php" class="menu-link">Peta</a>
+                <a href="kontak.php" class="menu-link">Kontak</a>
+            </nav>
         </div>
-        <nav class="menu-links">
-            <a href="beranda.php" class="menu-link">Beranda</a>
-            <a href="bbsejarah.php" class="menu-link">Bangunan Bersejarah</a>
-            <a href="gallery.php" class="menu-link">Galeri</a>
-            <a href="timeline.php" class="menu-link">Timeline</a>
-            <a href="quiz.php" class="menu-link">Kuis</a>
-            <a href="petawisata.php" class="menu-link">Peta</a>
-            <a href="kontak.php" class="menu-link">Kontak</a>
-        </nav>
     </div>
-</div>
+</nav>
